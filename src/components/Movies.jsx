bfa6423da96dcff7f432c278a10d4b8522fec1e9
Gunna9999 <emmanuelugochukwu2000@gmail.com>
2023-10-22 17:41:14 +0100

    commit

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -12,6 +12,7 @@ function Movies({id}) {
     const {toggle,inputValue} = useContext(Container);
     const input = inputValue;
     const Shown = input ? "search" : "discover";
+    const [loading, setLoading] = useState(true);
 
     const[moviesData,setMoviesData] = useState([])
 
@@ -31,49 +32,62 @@ function Movies({id}) {
   useEffect(()=> {
     MovieCall()
   },[input])
-  console.log(moviesData)
+  console.log(moviesData);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000); 
+  }, []);
 
 
     return (
       <Fragment>
-
-            <div className={toggle? "mainBgColor" : "secondaryBgColor"}>
-                <div className="movie_container">
-       {moviesData.map((movie) => (
-                      <Link to={`/${movie.id}`} key={movie.id} style={{textDecorationLine:"none"}}>
-                          <div className="container">
-                              <AiFillPlayCircle color="green" fontSize={40} id="playicon"/>
-                              <img src={movie.poster_path ? `${Images}${movie.poster_path}`: Noimg} alt="movie poster" />
-                              <h4 id={movie.title.length < 28 ? "" : "smallertext"} className={toggle? "mainColor" : "secondaryColor"} >{movie.title}</h4>
-                          </div>
-                      </Link>
-                  ))}
+      <div className={toggle ? "mainBgColor" : "secondaryBgColor"}>
+        <div className="movie_container">
+          {moviesData.map((movie) => (
+            loading ? (
+              <div className="loading-spinner"></div>
+            ) : (
+              <Link to={`/${movie.id}`} key={movie.id} style={{ textDecorationLine: "none" }}>
+                <div className="container">
+                  <AiFillPlayCircle color="green" fontSize={40} id="playicon" />
+                  <img src={movie.poster_path ? `${Images}${movie.poster_path}` : Noimg} alt="movie poster" />
+                  <h4 id={movie.title.length < 28 ? "" : "smallertext"} className={toggle ? "mainColor" : "secondaryColor"}>
+                    {movie.title}
+                  </h4>
+                </div>
+              </Link>
+            )
+          ))}  commit
         </div>
-       <div className="bottom_bg">
-         <img src={BottomBg} width="100%"  height="350px"/>
-         <div className="profile_link">
-          <div className="contact">
-             <h2>Connect Us</h2>
-          </div>
-          <div className="profile">
-         <a href="https://www.linkedin.com/in/ugochukwu-emmanuel-ba798a25a/">
-           <AiFillLinkedin color="#0072b1" fontSize={30} cursor="pointer"  fontWeight="bolder"/>
-         </a>
-          <a href="https://twitter.com/9Gunna9">
-            <AiOutlineTwitter color="	#1DA1F2" fontSize={30} fontWeight="bolder"/>
-          </a>
-         <a href="https://github.com/Dev-Emmyy">
-            <AiFillGithub color="black" fontSize={30} cursor="pointer"  fontWeight="bolder" />
-          </a>
-          </div>
+        <div className="bottom_bg">
+          <img src={BottomBg} width="100%" height="350px" />
+          <div className="profile_link">
+            <div className="contact">
+              <h2>Connect Us</h2>
+            </div>
+            <div className="profile">
+              <a href="https://www.linkedin.com/in/ugochukwu-emmanuel-ba798a25a/">
+                <AiFillLinkedin color="#0072b1" fontSize={30} cursor="pointer" fontWeight="bolder" />
+              </a>
+              <a href="https://twitter.com/9Gunna9">
+                <AiOutlineTwitter color="#1DA1F2" fontSize={30} fontWeight="bolder" />
+              </a>
+              <a href="https://github.com/Dev-Emmyy">
+                <AiFillGithub color="black" fontSize={30} cursor="pointer" fontWeight="bolder" />
+              </a>
+            </div>
 
-          <div>
-         <h2> My Challenge 1 project, Created by Dev-Emmy</h2>
-         </div>
-         </div>
-       </div>
+            <div>
+              <h2>My Challenge 1 project, Created by Dev-Emmy</h2>
+            </div>
+          </div>
         </div>
-         </Fragment>
+      </div>
+    </Fragment>
+
     )
 
 }
