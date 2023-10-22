15ac7f62c34fcc0bf5d2abac19eff65ad5190395
Gunna9999 <emmanuelugochukwu2000@gmail.com>
2023-10-22 18:12:26 +0100

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,10 +42,10 @@ function Navbar(){
            
 
             <div className="color_mode" onClick={() => setToggle(!toggle)}>
-                            <h3>{toggle? "DarkMode" : "WhiteMode"}</h3>
-                              <BiMoon fontSize={23} id="mode" color="#fff"  className={toggle?  "display" : "noDisplay"}/>
-                             <BsCloudSun fontSize={23} id="mode"  color="#fff"  className={toggle? "noDisplay" :   "display"}/>
-                        </div>
+                <h3>{toggle? "DarkMode" : "WhiteMode"}</h3>
+                    <BiMoon fontSize={23} id="mode" color="#fff"  className={toggle?  "display" : "noDisplay"}/>
+                    <BsCloudSun fontSize={23} id="mode"  color="#fff"  className={toggle? "noDisplay" :   "display"}/>
+            </div>
         </nav>
 
         <Routes>
@@ -55,6 +55,32 @@ function Navbar(){
             <Route path="Trends" element={<Trends/>}/>
              <Route path=":id" element={<Details/>}/>
         </Routes>
+
+
+        <div className="bottom_bg">
+          <img src={BottomBg} width="100%"  height="350px"/>
+         <div className="profile_link">
+          <div className="contact">
+             <h2>Connect Us</h2>
+          </div>
+          <div className="profile">
+         <a href="https://www.linkedin.com/in/ugochukwu-emmanuel-ba798a25a/">
+           <AiFillLinkedin color="#0072b1" fontSize={30} cursor="pointer"  fontWeight="bolder"/>
+         </a>
+          <a href="https://twitter.com/9Gunna9">
+            <AiOutlineTwitter color="	#1DA1F2" fontSize={30} fontWeight="bolder"/>
+          </a>
+         <a href="https://github.com/Dev-Emmyy">
+            <AiFillGithub color="black" fontSize={30} cursor="pointer"  fontWeight="bolder" />
+          </a>
+          </div>
+
+          <div>
+            <h2> My Challenge 1 project, Created by Dev-Emmy</h2>
+          </div>
+
+         </div>
+        </div>
        </Fragment>
        </Container.Provider>
     )
