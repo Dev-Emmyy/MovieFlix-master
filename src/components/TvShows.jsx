53bab3e84aac9e0aeece5ab16bc4f20b846a0ff7
Gunna9999 <emmanuelugochukwu2000@gmail.com>
2023-10-22 18:16:20 +0100

    commit

--- src/components/TvShows.jsx
+++ src/components/TvShows.jsx
@@ -35,40 +35,16 @@ function TvShows({id}) {
 
     return (
     <Fragment>
-            <div className={toggle? "mainBgColor" : "secondaryBgColor"}>
-                <div className="movie_container">
-                     {TvShowsData.map((shows) => (
-                     
-                          <div className="container">
-                              <AiFillPlayCircle color="green" fontSize={40} id="playicon"/>
-                              <img src={shows.poster_path ? `${Img}${shows.poster_path}`: NoImg} alt="movie poster" />
-                               <h4 id={shows.name.length < 28? "" : "smallertext"} className={toggle? "mainColor" : "secondaryColor"} >{shows.name}</h4>
-                          </div>
-                  ))}
-                  </div>
-        <div className="bottom_bg">
-         <img src={BottomBg} width="100%"  height="350px"/>
-         <div className="profile_link">
-          <div className="contact">
-             <h2>Connect Us</h2>
+    <div className={toggle? "mainBgColor" : "secondaryBgColor"}>
+        <div className="movie_container">
+            {TvShowsData.map((shows) => (
+                <div className="container">
+                    <AiFillPlayCircle color="green" fontSize={40} id="playicon"/>
+                    <img src={shows.poster_path ? `${Img}${shows.poster_path}`: NoImg} alt="movie poster" />
+                      <h4 id={shows.name.length < 28? "" : "smallertext"} className={toggle? "mainColor" : "secondaryColor"} >{shows.name}</h4>
+                </div>
+          ))}
           </div>
-          <div className="profile">
-         <a href="https://www.linkedin.com/in/ugochukwu-emmanuel-ba798a25a/">
-           <AiFillLinkedin color="#0072b1" fontSize={30} cursor="pointer"  fontWeight="bolder"/>
-         </a>
-          <a href="https://twitter.com/9Gunna9">
-            <AiOutlineTwitter color="	#1DA1F2" fontSize={30} fontWeight="bolder"/>
-          </a>
-         <a href="https://github.com/Dev-Emmyy">
-            <AiFillGithub color="black" fontSize={30} cursor="pointer"  fontWeight="bolder" />
-          </a>
-          </div>
-
-          <div>
-         <h2> My Challenge 1 project, Created by Dev-Emmy</h2>
-         </div>
-         </div>
-         </div>
         </div>
     </Fragment>
     )
